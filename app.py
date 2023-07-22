b4c532a5a5953949a675ce41c1883d9ca91d258a
Prakash <prakashope@gmail.com>
2023-07-22 11:58:51 +0530

    qa chain

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
 
 with st.sidebar:
     st.title("Pdf Chat 1.0")
@@ -46,7 +49,7 @@ def main():
             )
 
             text_chunks = text_splitter.split_text(text = text)
-            st.write(text_chunks)
+            # st.write(text_chunks)
 
             # creating embeddings of the text chunks
             embeddings = OpenAIEmbeddings()
@@ -62,6 +65,23 @@ def main():
             else:
                  with open(f"{pdf_name}.pkl","wb") as f:
                      pickle.dump(VectorStore , f)
+            # Accept user questions / Query
+
+            query = st.text_input("Ask Questions about your PDF Document:")
+
+            if query:
+                 docs = VectorStore.similarity_search(query=query , k=3)
+                 llm = OpenAI(temperature=0)
+                 chain = load_qa_chain(llm = llm , chain_type="stuff")
+                 response = chain.run(input_documents=docs , question=query)
+                 st.write(response)
+
+
+                 st.write(docs)
+
+                 
+
+            
 
                  
             
